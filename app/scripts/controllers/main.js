8b27dcebbd1c2c450b4ceb13ac1841f12c130973
Adam Koniuszewski <adam@imaginarydesign.co.uk>
2014-05-09 10:51:16 +0100

    Add test conf

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,9 +3,9 @@
 angular.module('angularAppApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = [
-      'Item 1',
-      'Item 2',
-      'Item 3'
+      // 'Item 1',
+      // 'Item 2',
+      // 'Item 3'
     ];
     $scope.addTodo = function () {
       $scope.todos.push($scope.todo);
